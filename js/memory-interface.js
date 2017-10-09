d3a72ac0a112cc74f01af9290f2f208a7d1055ae
javier jimenez and Keegan Ruebling <keegan@>
2017-10-09 14:00:11 -0700

    second commit for unifinshed app

---
+++ js/memory-interface.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function() {
+  $('.flashcard').click(function(e) {
+    var kids = $(e.currentTarget).children();
+    kids.toggle(800);
+    var kids2 = $(e.currentTarget).toggleClass("color");
+  });
+});
---
+++ js/memory.js
@@ -0,0 +1,6 @@
+var img = new Array();
+
+for(var i=0; i<9; i++){
+  img[i] = new Image();
+  img[i] = '<img src="'+`${i + 1}`+'.png">';
+}
